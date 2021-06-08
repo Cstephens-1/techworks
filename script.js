0d4b69cdf0a696a499ddf34d536351e633c6a38f
Cory <corystephens012@gmail.com>
2021-06-08 19:50:59 -0400

    updated index.html, style.css, and script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+
+email.addEventListener("click", function(){
+    alert("placeholder");
+})
+
+phone.addEventListener("click", function(){
+    alert("123.456.7890");
+})
