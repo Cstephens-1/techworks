d58851b370f90641daced6cf021f81be35b45ac0
Cory <corystephens012@gmail.com>
2021-06-16 19:52:49 -0400

    updated the index, js, and css files

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
 
-email.addEventListener("click", function(){
-    alert("placeholder");
-})
+const contact = document.getElementById("contact");
+const education = document.getElementById("education");
 
-phone.addEventListener("click", function(){
-    alert("123.456.7890");
-})
+contact.addEventListener("click", function(){
+    alert("someone@something.com\n123.456.7890");
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+    
+
