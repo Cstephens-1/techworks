ca7720c6d6e37f2488930fb01e4f11900659a508
Cory <corystephens012@gmail.com>
2021-06-28 15:13:03 -0400

    added pic zoom capability

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const contact = document.getElementById("contact");
+const pic1 = document.getElementById("pic1");
+const pic2 = document.getElementById("pic2");
+const pic3 = document.getElementById("pic3");
+
+
+pic1.addEventListener("click", function(){
+    pic1.classList.toggle("clickedPic")
+});
+
+pic2.addEventListener("click", function(){
+    pic2.classList.toggle("clickedPic")
+});
+
+pic3.addEventListener("click", function(){
+    pic3.classList.toggle("clickedPic")
+});
 
 contact.addEventListener("click", function(){
     alert("email: something@somewhere.com\n123.456.7890")
 });
+
+
+
+
+
+
+
+
+
+
